fc74963b293d1c0424d392917d3cf9753a13e95b
s8nclone <dayojnr.3@gmail.com>
2024-04-19 06:58:29 +0000

    more mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,12 @@
 export default const getListStudentIds = (students) => {
-  if (!Array.isArray(students)) {
-    return [];
+  const studentArr = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  const mappedArr = studentArr.map((student) => student.id);
+  if (Array.isArray(obj)) {
+    return mappedArr;
   }
-  return students.map((student) => student.id);
-};
+  return [];
+}
