9ef8daddd7f7ce467630de1cceb0fec70233ce81
s8nclone <dayojnr.3@gmail.com>
2024-04-28 10:15:59 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from "./3-currency.js"
+
+class Pricing {
+  constructor(amount = 0, currency = Currency){
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount = 0){
+    this._amount = amount;
+  }
+
+  get amount(){
+    return this._amount;
+  }
+
+  set currency(currency = ""){
+    this._currency = currency;
+  }
+
+  get currency(){
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
