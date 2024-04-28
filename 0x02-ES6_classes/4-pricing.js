e073a30f0dfd8599ec18e537da5970349265f050
s8nclone <dayojnr.3@gmail.com>
2024-04-28 10:22:55 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,24 +1,24 @@
-import Currency from "./3-currency.js"
+import Currency from "./3-currency"
 
 class Pricing {
-  constructor(amount = 0, currency = Currency){
+  constructor(amount = 0, currency = Currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-  set amount(amount = 0){
+  set amount(amount = 0) {
     this._amount = amount;
   }
 
-  get amount(){
+  get amount() {
     return this._amount;
   }
 
-  set currency(currency = ""){
+  set currency(currency = "") {
     this._currency = currency;
   }
 
-  get currency(){
+  get currency() {
     return this._currency;
   }
 
